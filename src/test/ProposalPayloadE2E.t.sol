858353ec82bf00d3fadc1a0e26ba7863c0aff5f5
efecarranza <fcarr004@fiu.edu>
2022-12-01 10:33:25 -0500

    update tests with helpers

--- src/test/ProposalPayloadE2E.t.sol
+++ src/test/ProposalPayloadE2E.t.sol
@@ -11,8 +11,6 @@ import {ProposalPayload} from "../ProposalPayload.sol";
 import {DeployMainnetProposal} from "../../script/DeployMainnetProposal.s.sol";
 
 contract ProposalPayloadE2ETest is Test {
-    address public constant AAVE_WHALE = 0x25F2226B597E8F9514B3F68F00f494cF4f286491;
-
     uint256 public proposalId;
 
     function setUp() public {
@@ -23,7 +21,7 @@ contract ProposalPayloadE2ETest is Test {
         ProposalPayload proposalPayload = new ProposalPayload();
 
         // Create Proposal
-        vm.prank(AAVE_WHALE);
+        vm.prank(GovHelpers.AAVE_WHALE);
         proposalId = DeployMainnetProposal._deployMainnetProposal(
             address(proposalPayload),
             0x344d3181f08b3186228b93bac0005a3a961238164b8b06cbb5f0428a9180b8a7 // TODO: Replace with actual IPFS Hash
