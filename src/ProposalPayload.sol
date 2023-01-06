6066b9d5fedf233e4e893c049af04fa9e402d08b
efecarranza <fcarr004@fiu.edu>
2023-01-06 10:45:15 -0500

    update template

--- src/ProposalPayload.sol
+++ src/ProposalPayload.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity ^0.8.15;
 
-import {IAaveEcosystemReserveController} from "./external/aave/IAaveEcosystemReserveController.sol";
+import {AaveMisc} from "@aave-address-book/AaveMisc.sol";
 import {AaveV2Ethereum} from "@aave-address-book/AaveV2Ethereum.sol";
 
 /**
